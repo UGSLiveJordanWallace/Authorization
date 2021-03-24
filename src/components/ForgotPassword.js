633004602a77d124127cbd0e4e1126b86aba360a
UGSLIVE <jjbosscore@gmail.com>
2021-03-23 22:36:25 -0400

    Firebase Authentication Commit

---
+++ src/components/ForgotPassword.js
@@ -0,0 +1,54 @@
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../context/AuthContext';
+import { Link } from 'react-router-dom';
+
+export default function ForgotPassword() {
+  const emailRef = useRef()
+  const { resetPassword } = useAuth()
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [message, setMessage] = useState('')
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      setMessage('')
+      setError('')
+      setLoading(true)
+      await resetPassword(emailRef.current.value)
+      setMessage('Check your inbox for futher instructions')
+    } catch (e) {
+      setError('Failed to reset password!')
+    }
+    setLoading(false)
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">The Forgotten Key</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          {message && <Alert variant="success">{message}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+             Reset Password
+            </Button>
+          </Form>
+        </Card.Body>
+        <div className="w-100 text-center mt-3">
+          <Link to="/login">Login</Link>
+        </div>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Do not have an account? <Link to="/signup">Sign Up!</Link>
+      </div>
+    </>
+  )
+}
